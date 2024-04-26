ef880cad2335fe3cfa876a0f48bdfcb7f93e77d3
Abdullah <abdullaharshed956@gmail.com>
2024-04-26 15:59:39 +0500

    project

---
+++ vue-start-spa/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <navbar
+    :pages="pages"
+    :active-page="activePage"
+    :nav-link-click="(index)=> activePage=index">
+    </navbar>
+  <page-viewer :page-to-display="pages[activePage]"></page-viewer>
+</template>
+<script>
+import  Navbar  from './components/Navbar.vue'
+import  PageViewer  from './components/PageViewer.vue'
+export default {
+  components: {
+    Navbar, PageViewer,
+  },
+    data() {
+        return {
+          activePage: 0,
+          pages: [
+            {
+              link: { text: "Home", url: "index.html" },
+              pageTitle: "Home Page",
+              content: "Home Page Content",
+            },
+            {
+              link: { text: "About", url: "about.html" },
+              pageTitle: "About Page",
+              content: "About Page Content",
+            },
+            {
+              link: { text: "Contact", url: "contact.html" },
+              pageTitle: "Contact Page",
+              content: "Contact Page Content",
+            },
+          ],
+        };
+      },
+}
+</script>
---
+++ vue-start-spa/src/components/PageViewer.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+      <h1>{{pageToDisplay.pageTitle}}</h1>
+      <p>{{pageToDisplay.content}}</p>
+    </div>
+</template>
+<script>
+export default {
+    props: ["pageToDisplay"],
+}
+</script>
